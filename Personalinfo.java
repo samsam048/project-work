83605f25ebb0fcc7c20ad537ec42d12e506147eb
Samsam <samsamsmall048@gmail.com>
2026-06-07 18:53:53 +0000

    Add files via upload

---
+++ Personalinfo.java
@@ -0,0 +1,26 @@
+public class Personalinfo{
+      public static void main(String   args[]){
+           String fullName ="Samuel Dela";
+            int age = 20;
+             char gender = 'M';
+               double hight = 1.90;
+float weight =68.9f;
+                   boolean Student = true;
+                    String phonenumber = "0549473817";
+                     String grandLevel = "200Level";
+                      String program = "Computer Science";
+System.out.println("====PERSONAL INFORMATION====");
+     System.out.println("FULL NAME:" + fullName);
+System.out.println("AGE:"+ age + "years");
+     System.out.println("GENDER:"+ gender);
+System.out.println("HIGHT:"+ hight + "m");
+     System.out.println("WEIGHT:"+ weight + "kg");
+System.out.println("STUDENT:"+ Student);
+      System.out.println("CONTACT INFO:"+ phonenumber);
+System.out.println("GRAND LEVEL:"+ grandLevel);     
+      System.out.println("PROGRAM:" + program);
+System.out.println("====END OF PERSONAL INFORMATION====");
+
+
+   }
+}
